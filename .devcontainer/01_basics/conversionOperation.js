351c87cbc6e9d650bf0e304f2abeddf787dcd51a
TariqCoder-code <64901260+TariqCoder-code@users.noreply.github.com>
2024-01-01 17:28:19 +0000

    conversionOperation

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,24 @@
+//let score ="33"
+// let score = "33abc" 
+let score = true
+// let score = null
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(score)
+
+
+//"33" =>33
+//"33abc" => NaN
+//true=>1; false=>0
+
+let isLoggedIn = 1
+let booleanIsoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsoggedIn);
+
+// 1=> true; 0=>false
+// "" => false
+// "acv" => true
