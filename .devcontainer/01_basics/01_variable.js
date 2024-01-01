d864f9bcad4e589cef245dba156d4168e9907c86
TariqCoder-code <64901260+TariqCoder-code@users.noreply.github.com>
2024-01-01 12:50:01 +0000

    02_datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountnumber = 21347
+let accountemail = "xyz@gmail.com"
+var accountpassword = 1234 
+//This is also a way to create variable but please not prefer this method
+accountcity="Delhi"
+let accountstate
+//single-line comment pattern
+// accountnumber=2134
+
+//Multi-line patter
+/*
+ prefer not to use var because it creates issues in block scope and functional scope.
+*/
+console.log(accountnumber)
+console.table([accountnumber,accountemail,accountpassword,accountcity,accountstate])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all js code as newer version 
+
+//alert(3+3) //note we are using node js not browser
+
+//Readability is important
+console.log(3+3)
+console.log("Tariq")
+
+//Ecmascript which decide standards and specification
+//link:https://ecma-international.org/publications-and-standards/standards/ecma-414/
+//Also refer MDN docs it is eay to understand
+
+let name ="Tariq"
+let age = 25
+let isLoggedIn = false
+
+//Datatypes
+//number => 2 to the pwer 53 (almost please check)
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value/object 
+//undefined=>undefined
+//symbol => used for identifying uniqueness (mostly used in react)
+
+//object
+
+console.log(typeof "Tariq")
+console.log(typeof null)
+console.log(typeof undefined)
